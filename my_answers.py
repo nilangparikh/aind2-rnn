86463d66120348f5292a5647e2ebbf9b7c7616b9
Nilang Parikh <nilangparikh@hotmail.com>
2018-01-14 16:27:33 -0500

    Complete problem #2.

--- my_answers.py
+++ my_answers.py
@@ -3,6 +3,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Activation
 import keras
 
 
@@ -37,31 +38,46 @@ def build_part1_RNN(window_size):
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
-    punctuation = ['!', ',', '.', ':', ';', '?']
+    punctuation = ['!', ',', '.', '?']
 
-    text = text.replace('\n', ' ')
-    text = text.replace('\r', ' ')
+    remove_chars = {'@', '^', '--', '-', ';', '{', '}', '+', '-', '<', '>', '\"', '#', '*', '=', '&', '(', ')', '\'',
+                    '\\', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '$', '%', '~', '_', '[', ']', '|', '`',
+                    ':', ' , ', ' ., ', ' .f. . ', ' .f. ', ' .e. . ', '\n', '\r'}
+
+    for ch in remove_chars:
+        if ch in text:
+            text = text.replace(ch, ' ')
+
+    text = text.replace(' .', '.')
+    text = text.replace(' s', 's')
+    text = text.replace('  ', ' ')
 
     return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
-    inputs = []
-    outputs = []
+    steps = ((len(text) - window_size) + 1) // step_size
+    inputs = [text[step * step_size:step * step_size + window_size] for step in range(steps)]
+    outputs = [text[step * step_size + window_size] for step in range(steps)]
 
-    n = 0
-    text_list = list(text)
-    text_len = len(text_list)
+    #n = 0
+    #text_list = list(text)
+    #text_len = len(text_list)
 
-    while n + window_size < text_len:
-        inputs.append(text_list[n:(n+window_size)])
-        outputs.append(text_list[n+window_size])
-        n += step_size
+    #while n + window_size < text_len:
+    #    inputs.append(text_list[n:(n+window_size)])
+    #    outputs.append(text_list[n+window_size])
+    #    n += step_size
 
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size, num_chars)))
+    model.add(Dense(num_chars, activation='linear'))
+    model.add(Activation('softmax'))
+
+    return model
